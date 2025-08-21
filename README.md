36f4c66e31ab9221a504f7d5a886048690e6dbee
Christian Niederreiter <niederreiter@baumgartner-automation.at>
2025-08-21 14:53:37 +0200

    make the script code generic, independent of OneNote

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rsync Backup
+
+## Usage example for Backup of OneNote backup files to a different location
+
+1. Run `schtasks /create /tn "Run OneNote Backup" /tr "wsl /home/user/Documents/tools/onenote-backup/onenote-backup.sh" /sc minute`.
+2. In Windows Task Scheduler, at the "General" tab in task properties, click "Run whether user is logged on or not", "Do not store password", "Hidden".
--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# (1) RUN schtasks /create /tn "Run OneNote Backup" /tr "wsl /home/user/Documents/tools/onenote-backup/onenote-backup.sh" /sc minute
-# (2) IN TASK SCHEDULER, at "General" tab in task properties, click "Run whether user is logged on or not", "Do not store password", "Hidden"
-
 set -e # exit on error
 
 ENV_FILE=''
@@ -10,7 +7,7 @@ if test "$1"
 then
   ENV_FILE="$1"
 else
-  DEFAULT_ENV_FILE="$( dirname $0 )/onenote-backup-settings.env"
+  DEFAULT_ENV_FILE="$( dirname $0 )/backup-settings.env"
   if test -f "$DEFAULT_ENV_FILE"
   then
     ENV_FILE="$DEFAULT_ENV_FILE"
